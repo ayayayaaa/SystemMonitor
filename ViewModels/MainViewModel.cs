6621240786e50dfa49f77af168cfe00be03a14fa
ayayayaaa <claristawjy@gmail.com>
2026-05-06 16:45:06 +0700

    feat: add MainViewModel with auto-refresh timer

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,97 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SystemMonitor.Models;
+using SystemMonitor.Services;
+
+namespace SystemMonitor.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly HardwareService _hardwareService;
+        private System.Timers.Timer _timer;
+
+        // CPU
+        private float _cpuUsage;
+        public float CpuUsage
+        {
+            get => _cpuUsage;
+            set { _cpuUsage = value; OnPropertyChanged(); }
+        }
+
+        // RAM
+        private float _ramUsage;
+        public float RamUsage
+        {
+            get => _ramUsage;
+            set { _ramUsage = value; OnPropertyChanged(); }
+        }
+
+        private float _ramUsedGB;
+        public float RamUsedGB
+        {
+            get => _ramUsedGB;
+            set { _ramUsedGB = value; OnPropertyChanged(); }
+        }
+
+        private float _ramTotalGB;
+        public float RamTotalGB
+        {
+            get => _ramTotalGB;
+            set { _ramTotalGB = value; OnPropertyChanged(); }
+        }
+
+        // Network
+        private float _downloadKBps;
+        public float DownloadKBps
+        {
+            get => _downloadKBps;
+            set { _downloadKBps = value; OnPropertyChanged(); }
+        }
+
+        private float _uploadKBps;
+        public float UploadKBps
+        {
+            get => _uploadKBps;
+            set { _uploadKBps = value; OnPropertyChanged(); }
+        }
+
+        // Disk
+        public ObservableCollection<DiskMetric> DiskMetrics { get; } = new();
+
+        public MainViewModel()
+        {
+            _hardwareService = new HardwareService();
+            _timer = new System.Timers.Timer(2000);
+            _timer.Elapsed += (s, e) => Refresh();
+            _timer.Start();
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            var cpu = _hardwareService.GetCpuMetric();
+            var ram = _hardwareService.GetRamMetric();
+            var net = _hardwareService.GetNetworkMetric();
+            var disks = _hardwareService.GetDiskMetrics();
+
+            CpuUsage = cpu.TotalUsage;
+            RamUsage = ram.UsagePercent;
+            RamUsedGB = ram.UsedGB;
+            RamTotalGB = ram.TotalGB;
+            DownloadKBps = net.DownloadKBps;
+            UploadKBps = net.UploadKBps;
+
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                DiskMetrics.Clear();
+                foreach (var d in disks)
+                    DiskMetrics.Add(d);
+            });
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
--- obj/Debug/net10.0-windows/SystemMonitor.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/SystemMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089d6d97c114979ae813494b4f56720f6377952b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+798d0287bad7aa4eb3a73fe40667e92a39cd16c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
